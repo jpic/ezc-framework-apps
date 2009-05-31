a1708938bb6b2e61827672f8cd83815839bce16c
James Pic <jamespic@gmail.com>
2009-05-31 15:44:04 +0200

    Initial import

---
+++ lib/views/root.php
@@ -0,0 +1,13 @@
+<?php
+class aiiRootView extends ezcMvcView
+{
+    function createZones( $layout )
+    {
+        $zones = array();
+        $zones[] = new ezcMvcTemplateViewHandler( 'menu', 'menu.ezt' );
+        $zones[] = new ezcMvcPhpViewHandler( 'content', '../templates/generic_greeting.php' );
+        $zones[] = new ezcMvcTemplateViewHandler( 'page_layout', 'layout.ezt' );
+        return $zones;
+    }
+}
+?>
