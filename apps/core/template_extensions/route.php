e79096e91753aecf11097e788a821686715660b4
jpic <jamespic@gmail.com>
2009-08-07 09:51:35 +0000

    Documented the reverse routing custom block

--- apps/core/template_extensions/route.php
+++ apps/core/template_extensions/route.php
@@ -22,6 +22,20 @@ class aiiCoreRouteCustomBlock implements ezcTemplateCustomBlock {
     }
 
 
+    /**
+     * Generates the string url for a route and arguments.
+     *
+     * The "to" string parameter is the route name.
+     * The "with" associative array parameter is the route arguments.
+     *
+     * Example usage:
+     * <code>
+     * {route "admin_create" with array( 'poClass' => 'yourClass' )}
+     * </code>
+     * 
+     * @param mixed $params array with "to" and eventually "with"
+     * @return string Generated url.
+     */
     public static function reverseRoute( $params )
     {
         $project = aiiProjectConfiguration::instance( );
