710ddb50f5c0a14604dcf9dcaa0f2b6304b0f774
jpic <jamespic@gmail.com>
2009-10-23 12:21:09 +0000

    Tested reconverting a persistent object definition to another persistent object definition

--- apps/converter/formatTest.php
+++ apps/converter/formatTest.php
@@ -1,10 +1,55 @@
 <?php
 require_once 'config.php';
+require_once 'source.php';
 require_once 'PHPUnit/Framework.php';
+
+# define( 'WRITE_ALL' , 1);
 
 class formatTest extends PHPUnit_Framework_TestCase
 {
-    public function testFail(  ) {
-        $this->fail( 'test' );
+    protected $fixturePath = '';
+
+    public function setUp()
+    {
+        $this->fixturePath = join( DIRECTORY_SEPARATOR, array( 
+            dirname( __FILE__ ),
+            'tests'
+        ));
     }
+
+    public function testConverter()
+    {
+        $directoryIterator = new DirectoryIterator( $this->fixturePath );
+        foreach( $directoryIterator as $testDirectory ) {
+            if ( $testDirectory->isdot(  ) ) continue;
+
+            $testIterator = new DirectoryIterator( $testDirectory->getPath(  ) . DIRECTORY_SEPARATOR . ( string )$testDirectory );
+            foreach( $testIterator as $test ) {
+                if ( $test->isdot(  ) ) continue;
+
+                $converter = call_user_func( array( 
+                    $this,
+                    'get' . ( string )$testDirectory . 'Converter'
+                ) );
+
+                $testPath = $test->getPath(  ) . DIRECTORY_SEPARATOR . ( string ) $test;
+                
+                include $testPath . DIRECTORY_SEPARATOR . 'run.php';
+
+                $expected = include $testPath . DIRECTORY_SEPARATOR . 'expected.php';
+
+                $this->assertEquals( $expected, $result, "$test failed" );
+            }
+        }
+    }
+
+    public function getPersistentObjectConverter(  ) {
+        $session = ezcPersistentSessionInstance::get(  );
+        $persistentObjectConverter = new aiiPersistentObjectDefinitionsConverter(
+            $session->definitionManager->fetchDefinition( 'aiiSitesClass' ),
+            $session->definitionManager
+        );
+        return $persistentObjectConverter;
+    }
+
 }
